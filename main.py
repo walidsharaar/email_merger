a10851640f94bddc901dd7fea5ea28bce4c65a6c
Sharaar <walidsharar@gmail.com>
2021-06-27 13:54:47 +0200

    generate list of invited names

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+# todo create a list of name
+
+with open("./input/names/invited_names.txt") as names_files:
+    names= names_files.read()
+    print(names)
