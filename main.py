ffa6ecc34e49df084e220d13b4ce5f0776acfa4b
Sharaar <walidsharar@gmail.com>
2021-06-27 14:20:09 +0200

    generate letters for each person

--- main.py
+++ main.py
@@ -7,9 +7,19 @@
 #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-
+PALCEHOLDER="[name]"
 # todo create a list of name
 
 with open("./input/names/invited_names.txt") as names_files:
-    names= names_files.read()
+    names= names_files.readlines()
     print(names)
+
+# todo read the letter
+with open("./input/letters/starting_letter.txt") as letter:
+    letter_contents = letter.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter= letter_contents.replace(PALCEHOLDER,stripped_name)
+
+        print(new_letter)
+
